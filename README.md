5e82cb1f23333f743a0340870c1291f22dfd45a6
Karolina kosiorowska <kaarolina.kosiorowska@gmail.com>
2019-05-13 13:27:13 +0200

    Added all API for get

--- README.md
+++ README.md
@@ -3,7 +3,14 @@
 ### Database
 
 How to create  db.sqlite file 
+
 sqlite3 db.sqlite < schema.sql
 
-### Get all movies
-http://127.0.0.1:5000/movies
+## REST API
+
+* [movies](http://127.0.0.1:5000/movies) - Get all movies
+* [users](http://127.0.0.1:5000/users) - Get all users
+* [seans](http://127.0.0.1:5000/seans) - Get all seans
+* [groups](http://127.0.0.1:5000/groups) - Get all groups
+
+
--- app.py
+++ app.py
@@ -20,5 +20,29 @@ def movies():
     return json.dumps([dict(movie)for movie in movies])  # create json object
 
 
+@app.route('/users', methods=['GET', 'POST'])
+def users():
+    db = create_connection()
+    data = db.execute('SELECT * FROM users ORDER BY id ;')
+    users = data.fetchall()
+    return json.dumps([dict(user)for user in users])
+
+
+@app.route('/seans', methods=['GET', 'POST'])
+def seans():
+    db = create_connection()
+    data = db.execute('SELECT * FROM seans ORDER BY id ;')
+    seans = data.fetchall()
+    return json.dumps([dict(s)for s in seans])
+
+
+@app.route('/groups', methods=['GET', 'POST'])
+def groups():
+    db = create_connection()
+    data = db.execute('SELECT * FROM groups ORDER BY id ;')
+    groups = data.fetchall()
+    return json.dumps([dict(group)for group in groups])
+
+
 if __name__ == '__main__':
     app.run(debug=True)
